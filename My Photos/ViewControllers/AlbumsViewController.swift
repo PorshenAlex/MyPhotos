7bd7b60d7c0f6bd3f7d43204c25741560b1d15ea
Alexey <Porshen@alexeys-imac.local>
2016-11-17 22:06:03 +0300

    Add viewControllers

---
+++ My Photos/ViewControllers/AlbumsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AlbumsViewController.swift
+//  My Photos
+//
+//  Created by Alexey Tyurin on 17/11/16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
+
+
---
+++ My Photos/ViewControllers/PhotoViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoViewController.swift
+//  My Photos
+//
+//  Created by Alexey Tyurin on 17/11/16.
+//  Copyright © 2016 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
